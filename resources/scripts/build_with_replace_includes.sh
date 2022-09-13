b2f78ad44d030082ee949dbdd9365ab932682ac2
Sven Sommer <rob.hoffmann@posteo.de>
2022-09-13 15:27:56 +0200

    rewrote script 5

--- resources/scripts/build_with_replace_includes.sh
+++ resources/scripts/build_with_replace_includes.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "$PWD"
 cd "$(dirname "$0")" || exit
-echo "$PWD"
 for filename in $(find ../docs -name '*.adoc'); do
     newFileName=${filename//-source/}
     asciidoctor-reducer -o ./../../docs/$newFileName $filename
